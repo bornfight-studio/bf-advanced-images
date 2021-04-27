04e22107979419501bbefd5302a6baaf75d96292
josip <josip.mucak@bornfight.com>
2021-04-27 09:23:52 +0200

    ver update

--- wp-advanced-images-plugin.php
+++ wp-advanced-images-plugin.php
@@ -8,14 +8,14 @@
  * that starts the plugin.
  *
  * @link              http://example.com
- * @since             3.1.0
+ * @since             4.1.0
  * @package           WP Advanced Images Plugin
  *
  * @wordpress-plugin
  * Plugin Name:       WP Advanced Images Plugin
  * Plugin URI:        https://github.com/bornfight/wp-advanced-images-plugin
  * Description:       Image optimization
- * Version:           3.1.0
+ * Version:           4.1.0
  * Author:            Josip Mucak
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
