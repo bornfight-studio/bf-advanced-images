c93b3f4c4135a6b6374d59e481eb94215d47dcc7
josip <josip.mucak@bornfight.com>
2021-11-16 18:11:37 +0100

    delete bfai options when uninstall plugin

--- uninstall.php
+++ uninstall.php
@@ -1,6 +1,10 @@
 <?php
 // If uninstall not called from WordPress, then exit.
-
 if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
+}
+
+// If plugin is uninstalled delete added options
+if ( get_option( 'bfai_unset_image_sizes' ) ) {
+	delete_option( 'bfai_unset_image_sizes' );
 }
