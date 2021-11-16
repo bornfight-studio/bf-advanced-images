c831ab69dc17e52de2dd673840469b73801d7870
josip <josip.mucak@bornfight.com>
2021-11-16 18:03:13 +0100

    bfai cli commands

--- app/core/BFImagesCLI.php
+++ app/core/BFImagesCLI.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-use bfAdvancedImages\cli\ImagesControllerCLI;
+use bfAdvancedImages\controller\BFImagesControllerCLI;
 use \WP_CLI;
 
 class BFImagesCLI {
@@ -15,6 +15,32 @@ class BFImagesCLI {
 	}
 
 	public function register_cli_commands(): void {
-		WP_CLI::add_command( 'bfai-delete-images', array( new ImagesControllerCLI(), 'delete_images' ) );
+		WP_CLI::add_command( 'bfai-delete-images', array(
+			new BFImagesControllerCLI(),
+			'delete_images'
+		), array(
+			'shortdesc' => esc_html( 'Command for deleting cached images' ),
+		) );
+
+		WP_CLI::add_command( 'bfai-is-dir-writable', array(
+			new BFImagesControllerCLI(),
+			'is_dir_writable'
+		), array(
+			'shortdesc' => esc_html( 'Check if upload directory is writable' ),
+		) );
+
+		WP_CLI::add_command( 'bfai-unset-default-image-sizes', array(
+			new BFImagesControllerCLI(),
+			'unset_default_image_sizes',
+		), array(
+			'shortdesc' => esc_html( 'Unset default image sizes' ),
+		) );
+
+		WP_CLI::add_command( 'bfai-delete-default-image-sizes-option', array(
+			new BFImagesControllerCLI(),
+			'delete_default_image_sizes_option',
+		), array(
+			'shortdesc' => esc_html( 'Delete default image sizes option' ),
+		) );
 	}
 }
